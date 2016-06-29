bd056c6cde8cbda4ea209603559bf374715ed95a
Nathan Souza <nathanpsouza@gmail.com>
2016-06-29 10:47:49 -0300

    Adjusts for product factory

--- sage-one/spec/factories/products.rb
+++ sage-one/spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :product do
-    
+    name { Faker::Commerce.product_name }
+    category { Faker::Commerce.department }
+    external_code '42'
+    description { Faker::Lorem.paragraph }
+    barcode '1234567890'
+    ncm '1234456'
+    unit { ['un', 'kg'].sample }
+    value { Faker::Commerce.price }
+    quantity { [*1..10].sample }
   end
 end
